80b3b3870de03601add51e4b0f677903f8ccf9b7
Dean Anderson <dben1182@gmail.com>
2024-04-15 13:50:20 -0600

    still broken

--- Final Project/least_squares_system_identification.py
+++ Final Project/least_squares_system_identification.py
@@ -26,12 +26,12 @@ from a_N_constructor import a_N_constructor
 x_signal, sampleRate = lib.load('Commanding_Image_Of_Christ.mp3')
 
 #gets a shortened version of the x_signal so we don't have to process 40 miutes worth of material
-shortenedLength = 100000
+shortenedSignalLength = 100000
 #we also start at another point to there is no dead period of the speech
 shortenedStartPoint = 55000
-x_signal_shortened = x_signal[shortenedStartPoint:(shortenedStartPoint+shortenedLength)]
+x_signal_shortened = x_signal[shortenedStartPoint:(shortenedStartPoint+shortenedSignalLength)]
 #reshapes x_signal_shortened to be a column vector
-x_signal_shortened = x_signal_shortened.reshape((shortenedLength, 1))
+x_signal_shortened = x_signal_shortened.reshape((shortenedSignalLength, 1))
 
 
 #-----------------------------------------------------
@@ -85,13 +85,13 @@ numCoefficients = N + M + 1
 y_signal = linearFilter(b_original, a_original, x_signal_shortened)
 
 #adds noise to the y_signal, as will happen with all signals in the future
-y_signal_noisy = np.zeros((shortenedLength, 1))
+y_signal_noisy = np.zeros((shortenedSignalLength, 1))
 
 #sets the mean and variance of the signal noise
 noiseMean = 0.0
 noiseVariance = 0.025
 #adds the noise to create the noisy signal
-for n in range(shortenedLength):
+for n in range(shortenedSignalLength):
     y_signal_noisy[n][0] = y_signal[n][0] + np.random.normal(loc=noiseMean, scale=noiseVariance)
 
 #sets the lower and upper limits of the plotting
@@ -112,7 +112,7 @@ plt.legend(['x', 'y', 'y with noise'])
 # %%
 
 #-------------------------------------------------------------------------------
-
+#initialization calculations
 #The stationary Kalman filter is used to recursively update the estimate of the coefficients
 
 #In order to effectively implement the kalman filter, we need to use the A_N constructor and get the first 10 or so samples
@@ -125,17 +125,118 @@ numSamplesInitial = 10
 #creates the initial A matrix 
 A_initialization = np.zeros((numSamplesInitial, numCoefficients))
 
+
+#creates the y initialization vector
+y_initial = y_signal[:numSamplesInitial,:]
+print("y_initial: \n", y_initial)
+
+#does the same for the A
+
+#iterates through and gets the A matrix
 for n in range(numSamplesInitial):
     #gets a_n for each iteration
-    a_N = a_N_constructor(x_signal, y_signal_noisy, M=M, N=N, n=n)
+    a_N = a_N_constructor(x_signal_shortened, y_signal_noisy, M=M, N=N, n=n)
 
     #adds a_N to the temporary matrix
     A_initialization[n,:] = a_N.T
 
-    #prints the a initialization
-    print(A_initialization)
+
+#gets the initial P0, which is (A_T @ A)**-1, which will serve to initialize our Kalman filter
+P_N = np.linalg.inv(A_initialization.T @ A_initialization)
+
+#gets the initial x_star, which is the batch least squares for our initial guess
+x_star = P_N @ A_initialization.T @ y_initial
 
 
+#prints out the P_N initial, and then prints out x_star initial
+print("P_N initial: \n", P_N)
+print("x_star_initial: \n", x_star)
+
+#gets the rank of P_N to see if it is invertible here
+P_N_initial_rank = np.linalg.matrix_rank(P_N)
+#gets the shape of P_N
+P_N_shape = np.shape(P_N)
+#gets the dimensionality of P_N
+P_N_dimensionality = P_N_shape[0]
+
+print("P_N_initial_rank: \n", P_N_initial_rank)
+print("P_N_shape: \n", P_N_shape)
+print("P_N_dimensionality: \n", P_N_dimensionality)
+
+#end initialization-----------------------------------------------------------------------------
 
 
+#Kalman Filter (Bayesian Estimator)-------------------------------------------------------------
+
+#for the remaining samples in the y array, we will perform the recursive kalman filter algorithm
+#using the initial P_N and the initial x_star which we just calculate
+
+#creates the A matrix
+A = np.zeros((shortenedSignalLength, numCoefficients))
+
+
+#iterates through the y's we haven't used yet from our initialization
+
+for n in range(shortenedSignalLength):
+    
+    #gets the next a_N for this iteration
+    a_N = a_N_constructor(x_signal_shortened, y_signal, M=M, N=N, n=n)
+
+    #adds this a_N to the already existing A matrix
+    A[n,:] = a_N.T
+
+    #gets the temp to help simplify the kalman gain
+    kn_helper = a_N.T @ P_N @ a_N
+
+    #gets the kalman gain
+    k_n = (P_N @ a_N)/(1.0 + kn_helper[0][0])
+
+    #updates the P matrix
+    P_N = P_N - k_n @ a_N.T @ P_N
+    
+    #gets the x_star_helper
+    x_star_helper = a_N.T @ x_star
+    #upates the x star estimate
+    x_star = x_star + k_n*(y_signal[0][0] - x_star_helper[0][0])
+
+
+print("x_star final: \n", x_star)
+print("actual coefficients: \n", coefficients)
+x_star_error = x_star - coefficients
+print("x star error: \n", x_star_error)
+
+#gets the batch error
+x_star_batch = np.linalg.inv(A.T @ A) @ A.T @ y_signal
+print("x star batch: \n", x_star_batch)
+batch_error = x_star_batch - coefficients
+print("batch error: \n", batch_error)
+
+#-----------------------------------------------------------------------------------------------
+
+print("A test: \n", A[8:12,:])
+
+
+# %%
+
+#tries again in another context
+
+A_2 = np.zeros((shortenedSignalLength, numCoefficients))
+
+for n in range(shortenedSignalLength):
+
+    #calls the a_N constructor
+    a_N = a_N_constructor(x=x_signal_shortened, y=y_signal, M=M, N=N, n=n)
+
+    A_2[n,:] = a_N.T
+
+
+#gets the new x_star
+x_star_2 = np.linalg.inv(A_2.T @ A_2) @ A_2.T @ y_signal
+
+print("A2 test: \n", A_2[8:12])
+
+print("x star 2: \n", x_star_2)
+
+error = x_star_2 - coefficients
+print("x_star_2 error: \n", error)
 # %%
